daf15b37b462f06ed9de97a052743de39ef93934
Eric <contreras.eric18@gmail.com>
2021-01-14 12:37:59 -0500

    commit tree 7a6616a3510a5552acad55bd57c137e6728d7c49

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  sqr = 0 
+  new_array = []
+  array.each do|num|
+    sqr = num ** 2
+    new_array << sqr
+  end
+  return new_array
 end
+
+#   sqr = 0 
+#   array.collect! { |num| num ** 2 }
+  
+# end 
